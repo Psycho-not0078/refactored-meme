60a68663248496a3a8e546dda6932556a048da23
Sathya Narayana Bhat <snbhat0708@gmail.com>
2025-05-23 10:22:07 +0530

    Basic Updates - Updated workflow to run on specific changes only - updated pdf renderer - updated packages - added default theme that mostly works

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 
+const docHeight = () => {
+  const body = document.body
+  const html = document.documentElement;
+  return Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+}
+
 (async () => {
 
   // Get type of source from process.argv, default to url
-    var htmlFilePath = "Resume-Latest.html"
+    var htmlFilePath = "Sathya_Bhat_Resume_Latest.html"
 
   // Create a browser instance
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox'],
+      timeout: 10000,
+    });
 
   // Create a new page
     const page = await browser.newPage();
@@ -17,13 +26,15 @@ const fs = require('fs');
 
   // To reflect CSS used for screens instead of print
     await page.emulateMediaType('screen');
+    // const height = await page.evaluate(docHeight);
 
-  // Downlaod the PDF
-    const pdf = await page.pdf({
-        path: `Resume-Latest.pdf`,
-        printBackground: true,
-        format: 'A4',
-    });
+
+    await page.pdf({ path: 'Sathya_Bhat_Resume_Latest.pdf', printBackground: true, margin: {
+      top: "10px",
+      right: "0px",
+      bottom: "15px",
+      left: "0px"
+  }})
 
   // Close the browser instance
     await browser.close();
